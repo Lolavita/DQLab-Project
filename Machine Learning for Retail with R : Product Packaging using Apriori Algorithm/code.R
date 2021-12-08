3aeeacebb71c55f8168d057658f6968c7ddd92b6
Lolavita <84948209+Lolavita@users.noreply.github.com>
2021-12-08 17:04:00 +0700

    Update code.R

--- Machine Learning for Retail with R : Product Packaging using Apriori Algorithm/code.R
+++ Machine Learning for Retail with R : Product Packaging using Apriori Algorithm/code.R
@@ -1,22 +1,28 @@
+#inisialisasi
 > library(arules)
 > transaksi_tabular <- read.transactions(file = "transaksi_dqlab_retail.tsv", format ="single", sep="\t", cols = c(1,2), skip=1)
+
+#statistik top 10
 > topitem <- itemFrequency(transaksi_tabular, type="absolute")
 > topitem <- sort(topitem, decreasing=TRUE)
 > topitem <- topitem [1:10]
 > topitem <- data.frame("Nama Produk" = names(topitem), "Jumlah"=topitem, row.names=NULL)
 > topitem
 
+#statistik bottom 10
 > bottomitem <- itemFrequency(transaksi_tabular, type="absolute")
 > bottomitem <- sort(bottomitem, decreasing = FALSE) 
 > bottomitem <- bottomitem[1:10]
 > bottomitem <-data.frame("Nama Produk"=names(bottomitem), "Jumlah"=bottomitem, row.names=NULL)
 > bottomitem
 
+#mendapatkan kombinasi produk yang menarik
 comb <- apriori(transaksi_tabular, parameter = list(supp = 10/length(transaksi_tabular), confidence =0.5, minlen=2, maxlen=3))
 result <- head(comb, n=10, by="lift")
 result
 inspect(result)
 
+#mencari paket produk yang bisa dipasangkan dengan item slow-moving
 smov <- apriori(transaksi_tabular, parameter=list(supp=10/length(transaksi_tabular), confidence = 0.1, minlen=2, maxlen=3))
 a <- subset(smov, rhs %in% "Tas Makeup")
 a1 <- head(sort(a, by="lift", decreasing = TRUE), 3)
